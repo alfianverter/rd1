710594948f357bc9e1c79f18878f77e0a7f7e357
12 <38290702+alfianverter@users.noreply.github.com>
2019-01-11 21:37:00 +0700

    Add files via upload

---
+++ bots.py
@@ -0,0 +1,33 @@
+import discord
+import youtube_dl
+from discord.ext import commands
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "-")
+
+players = {}
+
+@client.event
+async def on_ready():
+     print("Bot Ready")
+
+@client.command(pass_context=True)
+async def join(ctx):
+     channel = ctx.message.author.voice.voice_channel
+     await client.join_voice_channel(channel)
+
+@client.command(pass_context=True)
+async def leave(ctx):
+     server = ctx.message.server
+     voice_client = client.voice_client_in(server)
+     await voice_client.disconnect()
+
+@client.command(pass_context=True)
+async def play(ctx):
+     server = ctx.message.server
+     voice_client = client.voice_client_in(server)
+     player = await voice_client.create_ytdl_player('https://www.youtube.com/watch?v=1QQlUah25UI')
+     players[server.id] = player
+     player.start()
+
+client.run("NTMzMjc1MDQ5OTIzNDQ0NzM2.Dxoq6A.-dbp3m--QGayQp2Wv_1LTEjmPbA")
